83a18d92247736ff479da307f46b220b73589881
eenewbsauce <ryan.m.tracey@gmail.com>
2016-06-22 09:50:15 -0400

    working with TS array

--- src/app/components/title/index.ts
+++ src/app/components/title/index.ts
@@ -7,6 +7,28 @@ import {Component} from '@angular/core';
     styles: [require('./style.scss').toString()],
     template: require('./template.html')
 })
-export class TesterComponent {
 
+export class TesterComponent {
+  constructor() {
+    this.Heroes = [
+      new Hero(1, "Ryan"),
+      new Hero(2, "Tom"),
+      {
+        id: 3,
+        name: "Samantha"
+      }
+    ]
+  }
+
+  Heroes: Hero[];
+}
+
+class Hero {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  id: number;
+  name: string;
 }
